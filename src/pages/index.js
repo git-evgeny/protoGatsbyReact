cf5843aacab106d9228491e33c18456c26caad8f
Evgeny Ardashev <evgeny.ardashev@aviia.com.au>
2018-11-07 00:48:01 +1100

    changed text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Dashboards</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Random text</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
